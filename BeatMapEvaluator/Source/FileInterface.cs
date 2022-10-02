570d023d73f111709e6f6375935a4704c5e5aff9
Owen Cook <owenmckenziecookbusiness@gmail.com>
2022-10-02 11:39:03 -0400

    Added previous interpretation scripts

---
+++ BeatMapEvaluator/Source/FileInterface.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+using System.IO.Compression;
+
+namespace BeatMapEvaluator
+{
+    internal class FileInterface {
+        private static readonly string beatSaverAPI = "https://beatsaver.com/api/download/key/";
+        private static readonly HttpClient webClient = new HttpClient();
+
+        public static void unzipMap(string bsr, string targetDir) {
+            string zipFileName = bsr + ".zip";
+            string zipPath = Path.Combine(targetDir, zipFileName);
+            string extractDir = Path.Combine(targetDir, bsr);
+            UserConsole.Log("Unzipping map..");
+
+            //Extract zip and delete it.
+            try {
+                ZipFile.ExtractToDirectory(zipPath, extractDir);
+                File.Delete(zipPath);
+            } catch(Exception err) {
+                UserConsole.Log($"Error: {err.Message}");
+                throw;
+            }
+            UserConsole.Log("Map unzipped.");
+        }
+        public static async Task downloadBSR(string bsr, string targetDir) {
+            string zipFileName = bsr + ".zip";
+            string zipPath = Path.Combine(targetDir, zipFileName);
+            Uri urlHandle = new Uri(beatSaverAPI + bsr);
+            UserConsole.Log($"Fetching map {bsr}..");
+            try {
+                var res = await webClient.GetAsync(urlHandle);
+                res.EnsureSuccessStatusCode();
+                using(var stream = new FileStream(zipPath, FileMode.CreateNew)) {
+                    await res.Content.CopyToAsync(stream);
+                    await stream.FlushAsync();
+                    stream.Close();
+                }
+            } catch(Exception err) {
+                UserConsole.Log($"Error: {err.Message}");
+                throw;
+            }
+            unzipMap(bsr, targetDir);
+        }
+
+        public static void deleteDirFull(string dirPath) {
+            DirectoryInfo info = new DirectoryInfo(dirPath);
+            try {
+                foreach(FileInfo file in info.GetFiles())
+                    file.Delete();
+                foreach(DirectoryInfo dir in info.GetDirectories())
+                    dir.Delete(true);
+                Directory.Delete(dirPath);
+            } catch(Exception err) {
+                UserConsole.Log($"Error: {err.Message}");
+                throw;
+            }
+            UserConsole.Log("Removed temp directory.");
+        }
+    }
+}
---
+++ BeatMapEvaluator/Source/UserConsole.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatMapEvaluator
+{
+    internal class UserConsole {
+        private readonly static string logStampFormat = @"hh:mm:ss";
+        private static string[] consoleBuffer = new string[4];
+
+        public delegate void updateStringGUI(string ctx);
+        public static updateStringGUI? onConsoleUpdate = null;
+
+        public static void Log(string message) {
+            //Log to file with time stamp
+            string timeStamp = DateTime.Now.ToString(logStampFormat);
+            System.Diagnostics.Debug.WriteLine($"[{timeStamp}|LOG]: {message}");
+
+            //Shift all logs in buffer up and add incoming
+            for(int i = 0; i < consoleBuffer.Length-1; i++)
+                consoleBuffer[i] = consoleBuffer[i+1];
+            consoleBuffer[consoleBuffer.Length-1] = message;
+
+            //Build the log string from array
+            string logBuffer = "Console log:\n";
+            foreach(string line in consoleBuffer)
+                logBuffer += line + '\n';
+            
+            //Call GUI update
+            if(onConsoleUpdate != null)
+                onConsoleUpdate.Invoke(logBuffer);
+        }
+    }
+}
